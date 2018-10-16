2cb630722c889219af4740f4c9194dcffb585550
kadrie <kadrie_989@abv.bg>
2018-10-16 15:49:36 +0300

    My music factory,

--- W3InClass/MusicFactory/Program.cs
+++ W3InClass/MusicFactory/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,35 @@ namespace MusicFactory
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Play some music.");
+            Console.WriteLine("Do you want to play some music?");
+            string answer = Console.ReadLine();
+
+
+            if (answer=="Yes")
+            {
+             
+                using (SoundPlayer player = new SoundPlayer(@"C:\Users\fmi\Desktop\W3\PU_IntroCSharp_InClass_1801681029\W3InClass\MusicFactory\Music\Deeperise - One By One ft. Jabbar (online-audio-converter.com).wav"))
+                {
+                    Console.WriteLine("Now Playing...");
+                    player.PlaySync();
+                    while (true) ;
+                }
+            }
+            else if (answer=="No")
+            {
+                Console.WriteLine("Goodbye.");
+            }
+            else
+            {
+                Console.WriteLine("Your option is invalid.");
+            }
+            
+
+
+
+            
+            
+
         }
     }
 }
